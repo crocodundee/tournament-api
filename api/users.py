fec1fbe9b7b5ad0e5c72fd672362a51372e40678
Anastasiia Didan <anastasia.didan.dev@gmail.com>
2024-12-06 17:55:05 +0200

    fix: UserCreateSchema validation and custom validation responses

--- api/users.py
+++ api/users.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from sqlalchemy.orm import Session
 
 from db.session import get_session
@@ -14,11 +14,17 @@ def sign_up(user: UserCreateSchema, session: Session = Depends(get_session)):
     data = dict(user)
     user = UserModel.filter(session, UserModel.email == data["email"])
     if user:
-        return JSONResponse(
-            {"email": "User with this email already exists."}, status_code=400
+        raise RequestValidationError(
+            errors=[
+                {
+                    "loc": ["email"],
+                    "msg": "User with this email already exists.",
+                    "type": "value_error",
+                }
+            ]
         )
     return UserModel.create(
         session,
         email=data["email"],
-        password_hash=UserModel.hash_password(data["password"]),
+        password_hash=UserModel.hash_password(data["password1"]),
     )
--- schemas/user.py
+++ schemas/user.py
@@ -1,11 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, model_validator
+from typing_extensions import Self
 
 
 class UserCreateSchema(BaseModel):
-    email: str
-    password: str
+    email: EmailStr
+    password1: str
+    password2: str
+
+    @model_validator(mode="after")
+    def validate_passwords_match(self) -> Self:
+        if self.password1 == self.password2:
+            return self
+        raise ValueError("Passwords do not match")
 
 
 class UserSchema(BaseModel):
-    email: str
+    email: EmailStr
     is_active: bool
