efdf4eb443bd7560a0e8dc5ffc4c61096065cd60
Anastasiia Didan <a.didan@barva.tech>
2025-02-21 18:41:32 +0200

    fix: test v4

--- tests/api/test_users_views.py
+++ tests/api/test_users_views.py
@@ -1,6 +1,10 @@
 def test_registration(client):
     response = client.post(
-        "/users/sign-up",
-        json={"email": "test@email.com", "password": "defaultpass"}
+        '/users/sign-up',
+        json={
+            'email': 'test@email.com',
+            'password1': 'defaultpass',
+            'password2': 'defaultpass'
+        }
     )
-    assert response.status_code == 201
+    assert response.status_code == 201, response.json()
